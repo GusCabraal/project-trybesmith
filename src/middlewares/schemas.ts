e6747f93bb378f454bdda969d258ca0429a096cb
GusCabraal <gustavocabral97@hotmail.com>
2022-10-08 11:30:48 -0300

    refactor: funções com joi

--- src/middlewares/schemas.ts
+++ src/middlewares/schemas.ts
@@ -1,5 +1,10 @@
 import Joi from 'joi';
 
+const newLogin = Joi.object({
+  username: Joi.string().required(),
+  password: Joi.string().required(),
+});
+
 const newProduct = Joi.object({
   name: Joi.string().min(3).required(),
   amount: Joi.string().min(3).required(),
@@ -11,11 +16,19 @@ const newUser = Joi.object({
   level: Joi.number().min(1).required(),
   password: Joi.string().min(8).required(),
 });
+
 const productId = Joi.number().required();
 const updateProduct = Joi.object({
-  productsIds: Joi.array().items(productId).min(1).required(),
+  productsIds: Joi.array().items(productId).min(1)
+    .required()
+    .messages({
+      'array.base': '"productsIds" must be an array',
+      'array.includesRequiredUnknowns': '"productsIds" must include only numbers',
+    }),
 });
+
 export {
+  newLogin,
   newProduct,
   newUser,
   updateProduct,
--- src/middlewares/validateInputValues.ts
+++ src/middlewares/validateInputValues.ts
@@ -1,40 +1,34 @@
 import { Request, Response, NextFunction } from 'express';
-import { newProduct, newUser, updateProduct } from './schemas';
+import { newLogin, newProduct, newUser, updateProduct } from './schemas';
+
+const ANY_REQUIRED = 'any.required';
 
 const validateLogin = async (req:Request, res:Response, next:NextFunction) => {
   const { username, password } = req.body;
-  if (!username) {
-    const message = '"username" is required';
-    return res.status(400).json({ message });
-  }
-  if (!password) {
-    const message = '"password" is required';
-    return res.status(400).json({ message });
+  const { error } = newLogin.validate({ username, password });
+  if (error) {
+    return res.status(400).json({ message: error.message });
   }
   next();
 };
-const ANY_REQUIRED = 'any.required';
 
 const validateNewProduct = async (req:Request, res:Response, next:NextFunction) => {
   const { name, amount } = req.body;
   const { error } = newProduct.validate({ name, amount });
   if (error) {
-    if (error.details[0].type === ANY_REQUIRED) {
-      return res.status(400).json({ message: error.message });
-    }
-    return res.status(422).json({ message: error.message });
+    const status = error.details[0].type === ANY_REQUIRED ? 400 : 422;
+    return res.status(status).json({ message: error.message });
   }
   
   next();
 };
+
 const validateNewUser = async (req:Request, res:Response, next:NextFunction) => {
   const { username, level, classe, password } = req.body;
   const { error } = newUser.validate({ username, level, classe, password });
   if (error) {
-    if (error.details[0].type === ANY_REQUIRED) {
-      return res.status(400).json({ message: error.message });
-    }
-    return res.status(422).json({ message: error.message });
+    const status = error.details[0].type === ANY_REQUIRED ? 400 : 422;
+    return res.status(status).json({ message: error.message });
   }
   
   next();
@@ -43,14 +37,8 @@ const validateNewUser = async (req:Request, res:Response, next:NextFunction) => 
 const validateUpdateProduct = async (req:Request, res:Response, next:NextFunction) => {
   const { error } = updateProduct.validate(req.body);
   if (error) {
-    if (error.details[0].type === ANY_REQUIRED) {
-      return res.status(400).json({ message: error.message });
-    }
-    console.log(error);
-    if (error.details[0].type === 'array.includesRequiredUnknowns') {
-      return res.status(422).json({ message: '"productsIds" must include only numbers' });
-    }
-    return res.status(422).json({ message: '"productsIds" must be an array' });
+    const status = error.details[0].type === ANY_REQUIRED ? 400 : 422;
+    return res.status(status).json({ message: error.message });
   }
   
   next();
